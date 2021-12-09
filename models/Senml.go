2d732fcec43b32529483facfc78738c2a28ce06e
qs-wang <32129299+qs-wang@users.noreply.github.com>
2021-12-09 16:18:40 +0800

    Do not omit value for SenML (#18)

--- models/Senml.go
+++ models/Senml.go
@@ -3,16 +3,16 @@ package models
 type NanoTimestamp = float64
 
 type SenML struct {
-	N string        `json:"n"`           // Resource Name
-	T NanoTimestamp `json:"t"`           // Timestamp
-	V float32       `json:"v,omitempty"` // Value
-	U string        `json:"u"`           // Unit
-	L string        `json:"l"`           // Link
+	N string        `json:"n"` // Resource Name
+	T NanoTimestamp `json:"t"` // Timestamp
+	V float32       `json:"v"` // Value
+	U string        `json:"u"` // Unit
+	L string        `json:"l"` // Link
 }
 
 type BaseSenML struct {
 	N string  `json:"n,omitempty"` // Resource Name
-	V float32 `json:"v,omitempty"` // Value
+	V float32 `json:"v"`           // Value
 	U string  `json:"u,omitempty"` // Unit
 
 	BN   string        `json:"bn"`           // Prefix name
